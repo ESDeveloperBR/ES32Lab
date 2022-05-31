b0659318228d913a0f07f7e48740e37bf9698d35
Eder João Santini <ederjsantini@gmail.com>
2022-05-31 12:27:08 -0400

    Foi add comentários em toda a classe

--- DigitalButton.h
+++ DigitalButton.h
@@ -3,7 +3,7 @@
   WhatsApp +55 69 98463 9009
   Data de criação: 12/12/2018
   Ultima Alteração: 31/05/20222
-  Versão: 0.2.0
+  Versão: 0.2.1
 */
 
 #ifndef DigitalButton_H
@@ -14,42 +14,52 @@
 // <<<<<<<<<<<<<<< Classe Botão Externo >>>>>>>>>>>>>>
 class DigitalButton{
   private:
-    int     _pino;                  // Pino responsavel em habilitar ou não o drive do motor
-    boolean _btPress = false; // Botão pressionado inicia com false
+    int     _pin;             // Pino a ser utilizado pelo botão ou sensor
+    boolean _btPress = false; // Botão pressionado inicia com "false"
     
   public:
     // <<<<<<<< Método >>>>>>
-    void setPino(int pino); // Configura qual o pino de leitura
-    boolean hold();      // Retorna true interuptamente ao seguar o botão
-    boolean press();   // Retorna true ao pressionar
-    boolean release();       // Retorna true ao soltar
+    void setPino(int pino); // Inicializa o pino do botão
+    boolean hold();         // Retorna "true" enquanto o botão estiver pressionado
+    boolean press();        // Retorna "true" quando o botão for pressionado
+    boolean release();      // Retorna "true" quando o botão for solto
 };
-// <<<<<<<<< Configura qual pino será o botão >>>>>>>>>>
-void DigitalButton::setPino(int pino){
-  _pino = pino;
-  pinMode(_pino, INPUT_PULLUP);
+// <<<<<<<<<<<<<<<< Inicializa o pino do botão >>>>>>>>>>>>>>>>>>>>>>>>>>
+/* Initialize button pin.
+- pin: Address of the GPIO that is connected to the button or sensor
+*/
+void DigitalButton::setPino(int pin){
+  _pin = pin;
+  pinMode(_pin, INPUT_PULLUP);
 }
-// <<<<<<< Retorna true interuptamente ao seguar o botão >>>>>>>>>>>>>>>>
+// <<<<<<< Retorna "true" enquanto o botão estiver pressionado >>>>>>>>>>>>>>>>
+/* Returns "true" while the button is hold.
+*/
 boolean DigitalButton::hold(){
-  if( !digitalRead(_pino) ) { 
+  if( !digitalRead(_pin) ) { 
     return true;
   }
   return false;
 }
+// <<<<<<<<<<<<<<<<<<<<<< Retorna "true" quando o botão for pressionado >>>>>>>>>>>>>>>>>>>
+/* Returns "true" when the button is pressed.
+*/
 boolean DigitalButton::press(){
-  if( !digitalRead(_pino) & !_btPress ) { 
+  if( !digitalRead(_pin) & !_btPress ) { 
     _btPress = true;
     return true;
-  } else if( digitalRead(_pino) & _btPress ) {
+  } else if( digitalRead(_pin) & _btPress ) {
     _btPress = false;
   }
   return false;
 }
-// <<<<<<< Retorna true ao soltar >>>>>>>>>>>>
+// <<<<<<< Retorna "true" quando o botão for solto >>>>>>>>>>>>
+/* Returns "true" when the button is released.
+*/
 boolean DigitalButton::release(){
-  if( !digitalRead(_pino) & !_btPress ) {
+  if( !digitalRead(_pin) & !_btPress ) {
     _btPress = true;
-  } else if(digitalRead(_pino) & _btPress ){
+  } else if(digitalRead(_pin) & _btPress ){
     _btPress = false;
     return true;
   }

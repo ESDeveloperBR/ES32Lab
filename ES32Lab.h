db3e82cb82f6fb4e29290653db797ee9bfeee020
Eder João Santini <ederjsantini@gmail.com>
2022-06-02 16:37:23 -0400

    ES32Lab v.0.6.2 add drawString

--- ES32Lab.h
+++ ES32Lab.h
@@ -2,7 +2,7 @@
     #define _ES32LAB_h
 
     #include <Arduino.h>
-    #define ES32LAB_VERSION "0.6.1"
+    #define ES32LAB_VERSION "0.6.2"
 
     #define P_KEYBOARD      33
     #define P_LED_GREEN     17

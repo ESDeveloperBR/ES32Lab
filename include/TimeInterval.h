4242547f513a07fc8202c5db1b0e005289106be7
Eder João Santini <ederjsantini@gmail.com>
2022-06-03 19:14:00 -0400

    0.3.4 Mudança na estrutura da classe

--- include/TimeInterval.h
+++ include/TimeInterval.h
@@ -12,7 +12,7 @@
 
 #include <Arduino.h>
 
-#define TIMEINTERVAL_VERSION "0.3.2 update date 06/03/2022"  // mm/dd/yyyy
+#define TIMEINTERVAL_VERSION "0.3.4 update 06/03/2022"  // mm/dd/yyyy
 // <<<<<<<<<<<<<<< Classe Botão Externo >>>>>>>>>>>>>>
 class TimeInterval{
   private:
@@ -20,84 +20,78 @@ class TimeInterval{
     long _start_Micros = micros();
      
   public:
-    // <<<<<<<< Método >>>>>>
-    boolean intervalMillis(long millisTime);    // Retorna "true" quando o tempo em milissegundos tiver terminado
-    void    updateMillis();                     // Atualizar leitura de milissegundos para a contagem de tempo para o proximo intervalo
+    // <<<<<<<<< Retorna "true" quando o tempo em milissegundos tiver terminado >>>>>>>>>>
+    /*Returns "true" when the time in milliseconds has elapsed
+    - millisTime: Time in milliseconds
+    */
+    boolean intervalMillis(long millisTime){
+        long tempMillisAtual = millis();
 
-    boolean intervalMicros(long microsTime);    // Retorna "true" quando o tempo em microssegundos tiver terminado
-    void    updadeMicros();                     // Atualizar leitura de microssegundos para a contagem de tempo para o proximo intervalo
+        if( (tempMillisAtual - _start_Millis) >= millisTime ){
+            _start_Millis = millis();
+            return true;
+        }else{
+            return false;
+        }
+    }
 
-    void    stopwatchStartMillis();             // Inicializa o cronômetro em Milissegundos
-    long    stopwatchStopMillis();              // Parar cronômetro em Milissegundos e retornar o valor obtido
-
-    void    stopwatchStartMicros();             // Inicializa o cronômetro em Microssegundos
-    long    stopwatchStopMicros();              // Parar cronômetro em Microssegundos e retornar o valor obtido
-};
-// <<<<<<<<< Retorna "true" quando o tempo em milissegundos tiver terminado >>>>>>>>>>
-/*Returns "true" when the time in milliseconds has elapsed
-- millisTime: Time in milliseconds
-*/
-boolean TimeInterval::intervalMillis(long millisTime){
-    long tempMillisAtual = millis();
-
-    if( (tempMillisAtual - _start_Millis) >= millisTime ){
+    // <<<<<< Atualizar leitura de milissegundos para a contagem de tempo para o proximo intervalo >>>>>>>>>>>>>>>>>>>>>>
+    /* Update millisecond reading for time count for next interval
+    */
+    void updateMillis(){
         _start_Millis = millis();
-        return true;
-    }else{
-        return false;
     }
-}
-// <<<<<< Atualizar leitura de milissegundos para a contagem de tempo para o proximo intervalo >>>>>>>>>>>>>>>>>>>>>>
-/* Update millisecond reading for time count for next interval
-*/
-void TimeInterval::updateMillis(){
-    _start_Millis = millis();
-}
 
-// <<<<<<<<< Retorna "true" quando o tempo em microssegundos tiver terminado >>>>>>>>>>
-/* Returns "true" when the time in microseconds has elapsed
-- microsTime: Time in microseconds
-*/
-boolean TimeInterval::intervalMicros(long microsTime){
-    long tempMicrosAtual = micros();
+    // <<<<<<<<< Retorna "true" quando o tempo em microssegundos tiver terminado >>>>>>>>>>
+    /* Returns "true" when the time in microseconds has elapsed
+    - microsTime: Time in microseconds
+    */
+    boolean intervalMicros(long microsTime){
+        long tempMicrosAtual = micros();
 
-    if( (tempMicrosAtual - _start_Micros) >= microsTime ){
+        if( (tempMicrosAtual - _start_Micros) >= microsTime ){
+            _start_Micros = micros();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // <<<<<< Atualizar leitura de microssegundos para a contagem de tempo para o proximo intervalo >>>>>>>>>>>>>>>>>>>>>>
+    /* Update microsecond reading for time count for next interval
+    */
+    void updadeMicros(){
         _start_Micros = micros();
-        return true;
-    }else{
-        return false;
     }
-}
-// <<<<<< Atualizar leitura de microssegundos para a contagem de tempo para o proximo intervalo >>>>>>>>>>>>>>>>>>>>>>
-/* Update microsecond reading for time count for next interval
-*/
-void TimeInterval::updadeMicros(){
-    _start_Micros = micros();
-}
-// <<<<<<<<<< Inicializa o cronômetro em Milissegundos >>>>>>>>>>>>>>>
-/* Initializes the stopwatch in milliseconds
-*/
-void TimeInterval::stopwatchStartMillis(){
-    _start_Millis = millis();
-}
-// <<<<<<<<<<<<<< Parar cronômetro em Milissegundos e retornar o valor obtido >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-/* Stop stopwatch - Milliseconds and return the obtained value
-*/
-long TimeInterval::stopwatchStopMillis(){
-    return  millis() - _start_Millis; 
-}
-// <<<<<<<<<< Inicializa o cronômetro em Microssegundos >>>>>>>>>>>>>>>
-/* Initializes the stopwatch in microseconds
-*/
-void TimeInterval::stopwatchStartMicros(){
-    _start_Micros = micros();
-}
-// <<<<<<<<<<<<<< Parar cronômetro em Microssegundos e retornar o valor obtido >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-/* Stop stopwatch - Microseconds and return the obtained value
-*/
-long TimeInterval::stopwatchStopMicros(){
-    return  micros() - _start_Micros; 
-}
-// <<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>
+
+    // <<<<<<<<<< Inicializa o cronômetro em Milissegundos >>>>>>>>>>>>>>>
+    /* Initializes the stopwatch in milliseconds
+    */
+    void stopwatchStartMillis(){
+        _start_Millis = millis();
+    }
+
+    // <<<<<<<<<<<<<< Parar cronômetro em Milissegundos e retornar o valor obtido >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    /* Stop stopwatch - Milliseconds and return the obtained value
+    */
+    long stopwatchStopMillis(){
+        return  millis() - _start_Millis; 
+    }
+
+    // <<<<<<<<<< Inicializa o cronômetro em Microssegundos >>>>>>>>>>>>>>>
+    /* Initializes the stopwatch in microseconds
+    */
+    void stopwatchStartMicros(){
+        _start_Micros = micros();
+    }
+
+    // <<<<<<<<<<<<<< Parar cronômetro em Microssegundos e retornar o valor obtido >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    /* Stop stopwatch - Microseconds and return the obtained value
+    */
+    long stopwatchStopMicros(){
+        return  micros() - _start_Micros; 
+    }
+    // <<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>
+};
 
 #endif

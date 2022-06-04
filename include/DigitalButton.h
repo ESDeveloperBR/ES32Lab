7958d68aa6c14061abf4ad77cc0ac54b0cc1707c
Eder João Santini <ederjsantini@gmail.com>
2022-06-03 20:16:26 -0400

    0.2.4 - Mudança na estrutura da classe

--- include/DigitalButton.h
+++ include/DigitalButton.h
@@ -8,7 +8,7 @@
 
 #include <Arduino.h>
 
-#define DIGITALBUTTON_VERSION "0.2.2 update date 05/31/2022"
+#define DIGITALBUTTON_VERSION "0.2.4 update 06/03/2022"  // mm/dd/yyyy
 // <<<<<<<<<<<<<<< Classe Botão Externo >>>>>>>>>>>>>>
 class DigitalButton{
   private:
@@ -16,52 +16,50 @@ class DigitalButton{
     boolean _btPress = false; // Botão pressionado inicia com "false"
     
   public:
-    // <<<<<<<< Método >>>>>>
-    void setPino(int pino); // Inicializa o pino do botão
-    boolean hold();         // Retorna "true" enquanto o botão estiver pressionado
-    boolean press();        // Retorna "true" quando o botão for pressionado
-    boolean release();      // Retorna "true" quando o botão for solto
-};
-// <<<<<<<<<<<<<<<< Inicializa o pino do botão >>>>>>>>>>>>>>>>>>>>>>>>>>
-/* Initialize button pin.
-- pin: Address of the GPIO that is connected to the button or sensor
-*/
-void DigitalButton::setPino(int pin){
-  _pin = pin;
-  pinMode(_pin, INPUT_PULLUP);
-}
-// <<<<<<< Retorna "true" enquanto o botão estiver pressionado >>>>>>>>>>>>>>>>
-/* Returns "true" while the button is hold.
-*/
-boolean DigitalButton::hold(){
-  if( !digitalRead(_pin) ) { 
-    return true;
-  }
-  return false;
-}
-// <<<<<<<<<<<<<<<<<<<<<< Retorna "true" quando o botão for pressionado >>>>>>>>>>>>>>>>>>>
-/* Returns "true" when the button is pressed.
-*/
-boolean DigitalButton::press(){
-  if( !digitalRead(_pin) & !_btPress ) { 
-    _btPress = true;
-    return true;
-  } else if( digitalRead(_pin) & _btPress ) {
-    _btPress = false;
-  }
-  return false;
-}
-// <<<<<<< Retorna "true" quando o botão for solto >>>>>>>>>>>>
-/* Returns "true" when the button is released.
-*/
-boolean DigitalButton::release(){
-  if( !digitalRead(_pin) & !_btPress ) {
-    _btPress = true;
-  } else if(digitalRead(_pin) & _btPress ){
-    _btPress = false;
-    return true;
-  }
-  return false;
- }
+    // <<<<<<<<<<<<<<<< Inicializa o pino do botão >>>>>>>>>>>>>>>>>>>>>>>>>>
+    /* Initialize button pin.
+    - pin: Address of the GPIO that is connected to the button or sensor
+    */
+    void setPino(int pin){
+      _pin = pin;
+      pinMode(_pin, INPUT_PULLUP);
+    }
 
+    // <<<<<<< Retorna "true" enquanto o botão estiver pressionado >>>>>>>>>>>>>>>>
+    /* Returns "true" while the button is hold.
+    */
+    boolean hold(){
+      if( !digitalRead(_pin) ) { 
+        return true;
+      }
+      return false;
+    }
+
+    // <<<<<<<<<<<<<<<<<<<<<< Retorna "true" quando o botão for pressionado >>>>>>>>>>>>>>>>>>>
+    /* Returns "true" when the button is pressed.
+    */
+    boolean press(){
+      if( !digitalRead(_pin) & !_btPress ) { 
+        _btPress = true;
+        return true;
+      } else if( digitalRead(_pin) & _btPress ) {
+        _btPress = false;
+      }
+      return false;
+    }
+
+    // <<<<<<< Retorna "true" quando o botão for solto >>>>>>>>>>>>
+    /* Returns "true" when the button is released.
+    */
+    boolean release(){
+      if( !digitalRead(_pin) & !_btPress ) {
+        _btPress = true;
+      } else if(digitalRead(_pin) & _btPress ){
+        _btPress = false;
+        return true;
+      }
+      return false;
+    }
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+};
 #endif

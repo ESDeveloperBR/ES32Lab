626474a0c1255d3bde0d2d96fd747ddfca3035ca
Eder João Santini <ederjsantini@gmail.com>
2022-06-06 15:36:04 -0400

    Correção do exemplo

--- examples/TimeInterval/TimeInterval-intervalMicros/TimeInterval-intervalMicros.ino
+++ examples/TimeInterval/TimeInterval-intervalMicros/TimeInterval-intervalMicros.ino
@@ -8,10 +8,10 @@ ATTENTION: ES32Lab depends on the library download
     https://github.com/ederjsantini/TFT_eSPI_ES32Lab
     
 Objective:
-- Use the "TimeInterval" class that manages the time through the "micros()" command, not stopping the code in execution;
+- Use the "TimeInterval" class that manages the time through the "intervalMicros()" command, not stopping the code in execution;
 
 Objetivo:
-- Utilizar a classe "TimeInterval" que gerencia o tempo através do comando "micros()", não parando o código em execução;
+- Utilizar a classe "TimeInterval" que gerencia o tempo através do comando "intervalMicros()", não parando o código em execução;
 */
 
 #include <Arduino.h>

10258982810cbb14033de25ffc6712d402a752a1
Eder João Santini <ederjsantini@gmail.com>
2022-06-06 15:35:23 -0400

    Correção do exemplo

--- examples/TimeInterval/TimeInterval-intervalMillis/TimeInterval-intervalMillis.ino
+++ examples/TimeInterval/TimeInterval-intervalMillis/TimeInterval-intervalMillis.ino
@@ -8,10 +8,10 @@ ATTENTION: ES32Lab depends on the library download
     https://github.com/ederjsantini/TFT_eSPI_ES32Lab
     
 Objective:
-- Use the "TimeInterval" class that manages the time through the "milles()" command, not stopping the code in execution;
+- Use the "TimeInterval" class that manages the time through the "intervalMillis()" command, not stopping the code in execution;
 
 Objetivo:
-- Utilizar a classe "TimeInterval" que gerencia o tempo através do comando "milles()", não parando o código em execução;
+- Utilizar a classe "TimeInterval" que gerencia o tempo através do comando "intervalMillis()", não parando o código em execução;
 */
 
 #include <Arduino.h>
